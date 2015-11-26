ff64c948d8257b40ce5a1cf03b9c7d1a1804d44f
Alejandro Gallo <gallo@pccq05.fkf.mpg.de>
2015-11-26 20:12:18 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name="Show me your electrons",
+    version="0.0.1",
+    description="A little program to view VASP's OUTCAR electronic positions",
+    url="http://github.com/alejandrogallo/smye",
+    author="Alejandro Gallo",
+    license="MIT",
+    packages=["smye"],
+    test_suite="smye.tests",
+    zip_safe=False)
+
+#tests_require=["nose"],
