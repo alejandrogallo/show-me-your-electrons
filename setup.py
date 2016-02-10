c86277c22fd04567682ad0b4f5bbb3f985a003a4
Alejandro Gallo <gallo@pccq05.fkf.mpg.de>
2016-02-10 16:27:16 +0100

    added install req

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
-setup(name="Show me your electrons",
-    version="0.0.1",
-    description="A little program to view VASP's OUTCAR electronic positions",
-    url="http://github.com/alejandrogallo/smye",
-    author="Alejandro Gallo",
-    license="MIT",
-    packages=["smye"],
-    test_suite="smye.tests",
-    scripts=["bin/smye"],
-    zip_safe=False)
+setup(name           = "Show me your electrons",
+    version          = "0.0.1",
+    description      = "A little program to view VASP's OUTCAR electronic positions",
+    url              = "http://github.com/alejandrogallo/smye",
+    author           = "Alejandro Gallo",
+    license          = "MIT",
+    packages         = ["smye"],
+    install_requires = [],
+    test_suite       = "smye.tests",
+    scripts          = ["bin/smye"],
+    zip_safe         = False)
