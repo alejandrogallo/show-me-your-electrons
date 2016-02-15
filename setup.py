04236e8669aa74c407d09abf9500351eedbd4ec2
Alejandro Gallo <gallo@pccq05.fkf.mpg.de>
2016-02-15 16:54:48 +0100

    Modularized information

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 from setuptools import setup
+import smye
 
-setup(name           = "Show me your electrons",
-    version          = "0.0.1",
-    description      = "A little program to view VASP's OUTCAR electronic positions",
-    url              = "http://github.com/alejandrogallo/smye",
-    author           = "Alejandro Gallo",
-    license          = "MIT",
-    packages         = ["smye"],
-    install_requires = [],
-    test_suite       = "smye.tests",
-    scripts          = ["bin/smye"],
-    zip_safe         = False)
+setup(**smye.SETUP_INFO)
--- smye/__init__.py
+++ smye/__init__.py
@@ -8,3 +8,16 @@ LOGO = """
 (____/\_)(_/(__/  (____)
 
 """
+
+SETUP_INFO = dict(
+    name             = "Show me your electrons",
+    version          = "0.0.1",
+    description      = "A little tool to parse electronic configurations",
+    url              = "http://github.com/alejandrogallo/smye",
+    author           = "Alejandro Gallo",
+    license          = "MIT",
+    packages         = ["smye"],
+    install_requires = [],
+    test_suite       = "smye.tests",
+    scripts          = ["bin/smye"],
+    zip_safe         = False)
